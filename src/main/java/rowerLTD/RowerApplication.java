71e87d57dca3c81e4b9127f4f114a011d0bb1758
Piterm21 <piterm21@gmail.com>
2017-02-28 17:00:28 +0100

    initial

---
+++ src/main/java/rowerLTD/RowerApplication.java
@@ -0,0 +1,13 @@
+package rowerLTD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowerApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RowerApplication.class, args);
+	}
+}
